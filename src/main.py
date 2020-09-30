d1572b977742c394c189eeee5a4faa85743a6710
Bluemi <bruno.schilling@protonmail.ch>
2020-09-30 13:43:51 +0200

    implemented RenderUpdate

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import curses
+from enum import Enum
+
 import songsdatabase
 import config
 from audio_backend import AudioBackend
@@ -15,19 +17,31 @@ class Musicus:
         self.songs = songsdatabase.SongsDatabase.from_dir(SONGS_DIRECTORY)
         self.playing = False
 
-    def render(self, scr):
-        scr.clear()
-        for index, song in enumerate(self.songs.song_list):
-            if index >= curses.LINES:
-                break
-            if index == self.song_index:
-                color_pair = 2
-            elif index == self.cursor_index:
-                color_pair = 3
-            else:
-                color_pair = 1
-            scr.addstr(index, 0, song, curses.color_pair(color_pair))
-        scr.refresh()
+    def render_line(self, scr, line_index):
+        song = self.songs.song_list[line_index]
+        if line_index == self.song_index:
+            color_pair = 2
+        elif line_index == self.cursor_index:
+            color_pair = 3
+        else:
+            color_pair = 1
+        scr.addstr(line_index, 0, song, curses.color_pair(color_pair))
+
+    def render(self, scr, render_update):
+        """
+        :type render_update: RenderUpdate
+        """
+        if render_update.update_type == RenderUpdate.UpdateType.INIT:
+            scr.clear()
+            for index, song in enumerate(self.songs.song_list):
+                if index >= curses.LINES:
+                    break
+                self.render_line(scr, index)
+            scr.refresh()
+        else:
+            if render_update.update_type == RenderUpdate.UpdateType.CURSOR_MOVE:
+                self.render_line(scr, render_update.old_cursor_pos)
+                self.render_line(scr, render_update.new_cursor_pos)
 
     def inc_cursor_index(self):
         if self.cursor_index < len(self.songs.song_list):
@@ -57,6 +71,31 @@ class Musicus:
         return self.songs.song_list[self.song_index]
 
 
+class RenderUpdate:
+    class UpdateType(Enum):
+        INIT = 0
+        CURSOR_MOVE = 1
+
+    @staticmethod
+    def init():
+        return RenderUpdate(
+            RenderUpdate.UpdateType.INIT
+        )
+
+    @staticmethod
+    def cursor_move(old_cursor_pos, new_cursor_pos):
+        return RenderUpdate(
+            RenderUpdate.UpdateType.CURSOR_MOVE,
+            old_cursor_pos=old_cursor_pos,
+            new_cursor_pos=new_cursor_pos
+        )
+
+    def __init__(self, update_type, old_cursor_pos=None, new_cursor_pos=None):
+        self.update_type = update_type
+        self.old_cursor_pos = old_cursor_pos
+        self.new_cursor_pos = new_cursor_pos
+
+
 def main(stdscr: curses.window, logs):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -67,19 +106,24 @@ def main(stdscr: curses.window, logs):
 
     musicus = Musicus()
     audio_backend = AudioBackend()
-    ch = 1
+    render_update: RenderUpdate or None = RenderUpdate.init()
     while True:
-        if ch != 0:
-            musicus.render(stdscr)
+        if render_update is not None:
+            musicus.render(stdscr, render_update)
+            render_update = None
 
         try:
             ch = stdscr.getkey()
             if ch == 'q':
                 break
             elif ch == 'j':
-                musicus.inc_cursor_index()
+                old_cursor_pos = musicus.cursor_index
+                if musicus.inc_cursor_index():
+                    render_update = RenderUpdate.cursor_move(old_cursor_pos, musicus.cursor_index)
             elif ch == 'k':
-                musicus.dec_cursor_index()
+                old_cursor_pos = musicus.cursor_index
+                if musicus.dec_cursor_index():
+                    render_update = RenderUpdate.cursor_move(old_cursor_pos, musicus.cursor_index)
             elif ch == 'c':
                 if musicus.playing:
                     musicus.playing = False
@@ -90,7 +134,7 @@ def main(stdscr: curses.window, logs):
             else:
                 logs.append(ch)
         except curses.error:
-            ch = 0
+            pass
 
         # logs.append('playing: {}  is_playing: {}'.format(musicus.playing, audio_backend.is_playing()))
         if musicus.playing and not audio_backend.is_playing():
